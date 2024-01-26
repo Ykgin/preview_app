16998faa00570df51ef92b8771dc0d2d17390943
Ykgin <janvier20.yuyu.k@gmail.com>
2024-01-26 22:31:04 +0900

    image機能修正の経過途中

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
   const previewList = document.getElementById('previews');
   if (!postForm)return null;
-  console.log("previewが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="post[image]"]');
   fileField.addEventListener('change', function(e){
